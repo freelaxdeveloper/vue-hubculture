08ee11a90e467fe192181885e814035989497a55
FreeLax <Sanek_OS9@yahoo.com>
2018-09-02 05:34:14 +0300

    use mugen-scroll

--- src/pages/NewsComponent.vue
+++ src/pages/NewsComponent.vue
@@ -29,11 +29,10 @@
         </div>
       </div>
     </section>
-    <pagination 
-      :current="currentPage" 
-      :total="totalNews"
-      :per-page="perPage"
-    ></pagination>
+
+    <mugen-scroll :handler="getPosts" :should-handle="!loading">
+      loading...
+    </mugen-scroll>
   </div>
 </template>
 
@@ -41,10 +40,12 @@
 import Vue from 'vue'
 import axios from 'axios'
 import lodash from 'lodash'
+import MugenScroll from 'vue-mugen-scroll'
 
 Vue.component('pagination', require('../components/PaginationComponent.vue').default);
 
 export default {
+  components: {MugenScroll},
   data() {
     return {
       photos: [],
@@ -54,6 +55,7 @@ export default {
       perPage: 9,
       currentPage: 1,
       loaded: false,
+      loading: true,
     }
   },
   computed: {
@@ -66,29 +68,40 @@ export default {
         active: true
       }]
     },
+    nextPage() {
+      return this.currentPage + 1
+    },
   },
   watch: {
     '$route': 'fetchNews'
   },
   methods: {
+    getPosts() {
+      console.log('load page', this.nextPage)
+      this.loading = true
+      this.fetchNews(this.nextPage)
+    },
     fetchNews(page) {
       this.loaded = false
-      page = this.$route.params.page ? parseInt(this.$route.params.page) : 1
+      page = !this.loading && this.$route.params.page ? parseInt(this.$route.params.page) : page
       var options = {
         params: {
           offset: (page !== 1) ? page * this.perPage - this.perPage : page,
           limit: this.perPage
         }
       }
-
       axios.get('/articles/group/0/news', options).then((response) => {
 
-        // this.news = lodash._.shuffle(response.data.data.items)
-        this.news = response.data.data.items
+        if (this.loading) {
+          this.news = [...this.news, ...response.data.data.items]
+        } else {
+          this.news = response.data.data.items
+        }
+        
         this.totalNews = parseInt(response.data.data.total)
         this.currentPage = page
         this.loaded = true
-
+        this.loading = false
       }).then(function (error) {
 
       })
